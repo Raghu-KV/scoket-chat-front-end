dd97663fa7ce4ee2de1bdccadfa70338087d590c
Raghu-KV <raghunandanv19@gmail.com>
2023-04-23 19:01:54 +0530

    dd

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:4000");
 
 function Chat() {
-  return <div>Chat</div>;
+  const { name, room } = queryString.parse(useLocation().search);
+  const [currentMessage, setCurrentMessage] = useState("");
+
+  socket.emit("join_room", room);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        name: name,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+    }
+  };
+
+  useEffect(() => {
+    socket.on("recived_message", (data) => {
+      console.log(data);
+    });
+  }, [socket]);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen border-3 ">
+      <div className="w-2/3">
+        <div className="chat-header bg-gray-500 py-3 rounded-t-xl">
+          <p className=" text-center text-2xl font-bold text-white">
+            Live chat
+          </p>
+        </div>
+        <div className="chat-body h-[30rem] md:h-[40rem] border-2 border-gray-500"></div>
+        <div className="chat-footer flex mt-2">
+          <input
+            type="text"
+            placeholder="enter your message..."
+            onChange={(event) => setCurrentMessage(event.target.value)}
+            className="w-full block px-4 py-3 border-2 border-blue-500 focus:outline-none focus:border-blue-500 focus:ring-1 focus:drop-shadow-xl rounded-l-lg"
+          />
+          <button
+            className="px-3 md:px-10 bg-blue-500 text-white rounded-r-lg"
+            onClick={sendMessage}
+          >
+            send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,9 +1,49 @@
-import io from "socket.io-client";
+// import io from "socket.io-client";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const socket = io.connect("http://localhost:4000");
+// const socket = io.connect("http://localhost:4000");
 
 function Join() {
-  return <div>Join</div>;
+  const [name, setName] = useState("");
+  const [roomNum, setRoomNum] = useState(1);
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-200 ">
+      <div className="border-2 bg-gray-200 rounded-2xl w-5/6 md:w-1/2 drop-shadow-2xl">
+        <h1 className="font-bold text-3xl text-center mb-6 mt-16 text-gray-500">
+          {" "}
+          Scoket Chat
+        </h1>
+
+        <input
+          type="text"
+          placeholder="Enter your username..."
+          className="w-5/6 mx-auto block bg-gray-200 border-2 border-gray-500 px-4 py-3 rounded-lg focus:outline-none focus:border-blue-500 focus:border-2 focus:ring-1 focus:drop-shadow-xl  mb-5  text-gray-500 text-lg duration-100"
+          onChange={(event) => setName(event.target.value)}
+          value={name}
+        />
+
+        <input
+          type="number"
+          placeholder="Enter your room number..."
+          className="w-5/6 mx-auto block bg-gray-200 border-2 border-gray-500 px-4 py-3 rounded-lg focus:outline-none focus:border-blue-500 focus:border-2 focus:ring-1 focus:drop-shadow-xl mb-5 text-gray-500 text-lg duration-100"
+          onChange={(event) => setRoomNum(event.target.value)}
+          value={roomNum}
+        />
+        <Link
+          to={`/chat?name=${name}&room=${roomNum}`}
+          onClick={(event) =>
+            !name || !roomNum ? event.preventDefault() : null
+          }
+        >
+          <button className="w-5/6 bg-blue-500 block mx-auto text-white px-4 py-3 rounded-lg mb-16 text-lg hover:bg-blue-600 duration-150">
+            Go to Chat!{" "}
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Join;
